a174ae4b4cfa5fc52140a6ed3069017dc6c3e456
TejeswarBharath <tneelam@miraclesoft.com>
2024-10-20 12:48:59 +0530

    Initial Code Changes

---
+++ src/main/java/com/example/vultrbackendservices/VultrbackendservicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.vultrbackendservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VultrbackendservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VultrbackendservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vultrbackendservices/VultrbackendservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vultrbackendservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VultrbackendservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
